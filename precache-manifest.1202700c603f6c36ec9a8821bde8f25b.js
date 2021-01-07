a6eba654977b8e19a54cba7cf3abeeba2ed16ff6
shobhit.singh <shobhit.singh@flipkart.com>
2021-01-07 21:53:42 +0530

    Updates

---
+++ precache-manifest.1202700c603f6c36ec9a8821bde8f25b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "433db308aef82304eaa7",
+    "url": "/playing-cards-master/static/css/main.a264a108.chunk.css"
+  },
+  {
+    "revision": "433db308aef82304eaa7",
+    "url": "/playing-cards-master/static/js/main.95efb413.chunk.js"
+  },
+  {
+    "revision": "e50498e8d6facf189c14",
+    "url": "/playing-cards-master/static/js/runtime~main.56fe1d29.js"
+  },
+  {
+    "revision": "d034ef17be3f4eb93415",
+    "url": "/playing-cards-master/static/js/2.f278bab1.chunk.js"
+  },
+  {
+    "revision": "69b3940bd4ae7bfd8e8652dcd57e7ced",
+    "url": "/playing-cards-master/static/media/heart.69b3940b.png"
+  },
+  {
+    "revision": "a5654b42a5eea0b85c879693a815c6d2",
+    "url": "/playing-cards-master/static/media/diamond.a5654b42.png"
+  },
+  {
+    "revision": "2a6241117edd8c869a89d07c1f0232c7",
+    "url": "/playing-cards-master/static/media/club.2a624111.png"
+  },
+  {
+    "revision": "c9111145773fb0dd7bfc2d5d55bff327",
+    "url": "/playing-cards-master/static/media/spade.c9111145.png"
+  },
+  {
+    "revision": "bf64c56256d7ea01c0f1def3f9690f43",
+    "url": "/playing-cards-master/index.html"
+  }
+];
