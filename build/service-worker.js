85069cac9aafcdb1a02b2dbff94e939e7476cec6
shobhit.singh <shobhit.singh@flipkart.com>
2021-01-07 21:56:30 +0530

    init

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1dbb214248bf39bb8b4b00d1de7661b2.js"
+  "/playing-cards-master/precache-manifest.1202700c603f6c36ec9a8821bde8f25b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/playing-cards-master/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
